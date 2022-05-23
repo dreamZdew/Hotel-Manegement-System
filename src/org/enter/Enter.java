dab0ae571756d30133c97b52ab3120b2b51c9119
dreamZdew <dreamZdew@outlook.com>
2022-05-23 11:18:03 +0800

    polish

--- src/org/enter/Enter.java
+++ src/org/enter/Enter.java
@@ -13,6 +13,11 @@ import org.eclipse.wb.swt.SWTResourceManager;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.layout.FormLayout;
+import org.eclipse.swt.layout.RowLayout;
+import org.eclipse.swt.layout.FormData;
+import org.eclipse.swt.layout.FormAttachment;
 
 
 
@@ -61,74 +66,113 @@ public class Enter {
 		shell = new Shell();
 		shell.setSize(800, 450);
 		shell.setText("欢迎进入酒店管理系统");
+		shell.setLayout(new FormLayout());
 		
-		text_username = new Text(shell, SWT.BORDER);
-		text_username.setBounds(358, 197, 126, 23);
+		Composite composite = new Composite(shell, SWT.NONE);
+		composite.setLayout(new FormLayout());
+		FormData fd_composite = new FormData();
+		fd_composite.bottom = new FormAttachment(0, 394);
+		fd_composite.right = new FormAttachment(0, 778);
+		fd_composite.top = new FormAttachment(0);
+		fd_composite.left = new FormAttachment(0);
+		composite.setLayoutData(fd_composite);
 		
-		text_password = new Text(shell, SWT.BORDER);
-		text_password.setBounds(358, 258, 126, 23);
+		text_username = new Text(composite, SWT.BORDER);
+		FormData fd_text_username = new FormData();
+		fd_text_username.bottom = new FormAttachment(0, 222);
+		fd_text_username.right = new FormAttachment(0, 484);
+		fd_text_username.left = new FormAttachment(0, 358);
+		text_username.setLayoutData(fd_text_username);
 		
-		Label label_username = new Label(shell, SWT.NONE);
+		text_password = new Text(composite, SWT.BORDER);
+		FormData fd_text_password = new FormData();
+		fd_text_password.bottom = new FormAttachment(0, 281);
+		fd_text_password.right = new FormAttachment(0, 484);
+		fd_text_password.left = new FormAttachment(0, 358);
+		text_password.setLayoutData(fd_text_password);
+		
+		Label label_username = new Label(composite, SWT.NONE);
+		fd_text_username.top = new FormAttachment(label_username, 0, SWT.TOP);
+		FormData fd_label_username = new FormData();
+		fd_label_username.bottom = new FormAttachment(0, 231);
+		fd_label_username.right = new FormAttachment(0, 352);
+		fd_label_username.top = new FormAttachment(0, 199);
+		fd_label_username.left = new FormAttachment(0, 274);
+		label_username.setLayoutData(fd_label_username);
 		label_username.setAlignment(SWT.CENTER);
 		label_username.setFont(SWTResourceManager.getFont("黑体", 12, SWT.NORMAL));
-		label_username.setBounds(274, 199, 78, 32);
 		label_username.setText("用户名");
 		
-		Label label_password = new Label(shell, SWT.NONE);
+		Label label_password = new Label(composite, SWT.NONE);
+		fd_text_password.top = new FormAttachment(label_password, 0, SWT.TOP);
+		FormData fd_label_password = new FormData();
+		fd_label_password.bottom = new FormAttachment(0, 290);
+		fd_label_password.right = new FormAttachment(0, 352);
+		fd_label_password.top = new FormAttachment(0, 258);
+		fd_label_password.left = new FormAttachment(0, 274);
+		label_password.setLayoutData(fd_label_password);
 		label_password.setText("密码");
 		label_password.setFont(SWTResourceManager.getFont("黑体", 12, SWT.NORMAL));
 		label_password.setAlignment(SWT.CENTER);
-		label_password.setBounds(274, 258, 78, 32);
 		
-		Label label_title = new Label(shell, SWT.NONE);
+		Label label_title = new Label(composite, SWT.NONE);
+		FormData fd_label_title = new FormData();
+		fd_label_title.bottom = new FormAttachment(0, 130);
+		fd_label_title.right = new FormAttachment(0, 611);
+		fd_label_title.top = new FormAttachment(0, 10);
+		fd_label_title.left = new FormAttachment(0, 167);
+		label_title.setLayoutData(fd_label_title);
 		label_title.setAlignment(SWT.CENTER);
 		label_title.setFont(SWTResourceManager.getFont("宋体", 50, SWT.NORMAL));
-		label_title.setBounds(170, 49, 444, 89);
-		label_title.setText("北气宾馆");
+		label_title.setText("西气宾馆");
 		
-		Label warn = new Label(shell, SWT.NONE);
+		Label warn = new Label(composite, SWT.NONE);
+		FormData fd_warn = new FormData();
+		fd_warn.bottom = new FormAttachment(0, 252);
+		fd_warn.right = new FormAttachment(0, 650);
+		fd_warn.top = new FormAttachment(0, 229);
+		fd_warn.left = new FormAttachment(0, 128);
+		warn.setLayoutData(fd_warn);
 		warn.setAlignment(SWT.CENTER);
 		warn.setForeground(SWTResourceManager.getColor(SWT.COLOR_RED));
 		warn.setFont(SWTResourceManager.getFont("黑体", 12, SWT.NORMAL));
-		warn.setBounds(267, 229, 250, 23);
 		
-		Button button_signup = new Button(shell, SWT.NONE);
-		button_signup.addSelectionListener(new SelectionAdapter() {//按下注册按钮跳转到注册界面
-			@Override
-			public void widgetSelected(SelectionEvent e) {
-				
-				shell.close();
-				try {
-					Display display = Display.getDefault();
-					Signpage signpage = new Signpage(display);
-					signpage.open();
-					signpage.layout();
-					while (!signpage.isDisposed()) {
-						if (!display.readAndDispatch()) {
-							display.sleep();
-						}
-					}
-				} catch (Exception ee) {ee.printStackTrace();}
-				
-			}
-		});
 		
-		Button button_login = new Button(shell, SWT.NONE);
+		Composite composite_Button = new Composite(composite, SWT.NONE);
+		FormData fd_composite_Button = new FormData();
+		fd_composite_Button.bottom = new FormAttachment(0);
+		fd_composite_Button.right = new FormAttachment(0);
+		fd_composite_Button.top = new FormAttachment(0);
+		fd_composite_Button.left = new FormAttachment(0);
+		composite_Button.setLayoutData(fd_composite_Button);
+		FormData fd_composite_Button1 = new FormData();
+		fd_composite_Button1.right = new FormAttachment(0, 569);
+		fd_composite_Button1.top = new FormAttachment(0, 319);
+		fd_composite_Button1.left = new FormAttachment(0, 209);
+		composite_Button.setLayoutData(fd_composite_Button1);
+				RowLayout rl_composite_Button = new RowLayout(SWT.HORIZONTAL);
+				rl_composite_Button.wrap = false;
+				rl_composite_Button.pack = false;
+				rl_composite_Button.justify = true;
+				composite_Button.setLayout(rl_composite_Button);
+		
+		Button button_login = new Button(composite_Button, SWT.NONE);
+		button_login.setBounds(274, 338, 80, 27);
 		button_login.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent e) {//明文存储用户名和密码
 				
 				String username=text_username.getText();
 				String password=text_password.getText();
-				
+				if(!username.equals("")&&!password.equals("")) {
 				try {//此处数据库参考数据库课本P190
 
 					conn=AboutDB.loginDB();
 					Statement Stmt = conn.createStatement();
-					String sqlQuery="select * from UserPass";//查询指令
+					String sqlQuery="select 用户名,密码 from UserPass where 用户名="+username+" and 密码 = "+password+" ";//查询指令
 					ResultSet rs=Stmt.executeQuery(sqlQuery);
 					while(rs.next()) {
-						if(username.equals(rs.getString(1))&&password.equals(rs.getString(2))){//登录判定 可以用的用户名密码有 test 123和test2 123
+						if(username.equals(rs.getString(1))&&password.equals(rs.getString(2))){//登录判定 
 							rs.close();Stmt.close();conn.close();
 							shell.close();
 							
@@ -150,7 +194,7 @@ public class Enter {
 							
 						}
 					}
-					if(text_username.getVisible()==true) {
+					if(!shell.isDisposed()) {
 						warn.setText("登陆失败，用户名或密码不正确");
 						text_username.setFocus();
 						
@@ -168,18 +212,40 @@ public class Enter {
 				
 					
 				}catch(Exception ee) {warn.setText("无法连接到服务器");}
-				
+				}else {warn.setText("用户名或密码不能为空");Runnable timer = new Runnable() {@Override public void run() {if(!shell.isDisposed())warn.setText("");}};Display.getDefault().timerExec(3000, timer);}
 			}
 		});
 		
 		button_login.setFont(SWTResourceManager.getFont("黑体", 11, SWT.NORMAL));
-		button_login.setBounds(274, 338, 80, 27);
 		button_login.setText("登录");
+		
+		Button button_signup = new Button(composite_Button, SWT.NONE);
+		button_signup.setBounds(404, 338, 80, 27);
+		button_signup.addSelectionListener(new SelectionAdapter() {//按下注册按钮跳转到注册界面
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				
+				shell.close();
+				try {
+					Display display = Display.getDefault();
+					Signpage signpage = new Signpage(display);
+					signpage.open();
+					signpage.layout();
+					while (!signpage.isDisposed()) {
+						if (!display.readAndDispatch()) {
+							display.sleep();
+						}
+					}
+				} catch (Exception ee) {ee.printStackTrace();}
+				
+			}
+		});
 		
 
 		button_signup.setText("注册");
 		button_signup.setFont(SWTResourceManager.getFont("黑体", 11, SWT.NORMAL));
-		button_signup.setBounds(404, 338, 80, 27);
+		
+		
 
 	}
 }
--- src/org/enter/Signpage.java
+++ src/org/enter/Signpage.java
@@ -14,6 +14,13 @@ import org.eclipse.wb.swt.SWTResourceManager;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.layout.FormLayout;
+import org.eclipse.swt.layout.FormData;
+import org.eclipse.swt.layout.FormAttachment;
+import org.eclipse.swt.layout.RowLayout;
+import org.eclipse.swt.layout.RowData;
 
 /*注册界面*/
 /*这个注册是给管理员注册*/
@@ -59,120 +66,167 @@ public class Signpage extends Shell {
 	protected void createContents() {
 		setText("注册界面");
 		setSize(800, 450);
+		setLayout(new FillLayout(SWT.VERTICAL));
 		
-		text_username = new Text(this, SWT.BORDER);
-		text_username.setBounds(358, 197, 126, 23);
+		Composite composite = new Composite(this, SWT.NONE);
+		composite.setLayout(new FormLayout());
 		
-		text_password = new Text(this, SWT.BORDER);
-		text_password.setBounds(358, 258, 126, 23);
+		text_username = new Text(composite, SWT.BORDER);
+		FormData fd_text_username = new FormData();
+		fd_text_username.bottom = new FormAttachment(0, 222);
+		fd_text_username.right = new FormAttachment(0, 484);
+		fd_text_username.left = new FormAttachment(0, 358);
+		text_username.setLayoutData(fd_text_username);
 		
-		Label label_username = new Label(this, SWT.NONE);
+		text_password = new Text(composite, SWT.BORDER);
+		FormData fd_text_password = new FormData();
+		fd_text_password.bottom = new FormAttachment(0, 281);
+		fd_text_password.right = new FormAttachment(0, 484);
+		fd_text_password.left = new FormAttachment(0, 358);
+		text_password.setLayoutData(fd_text_password);
+		
+		Label label_username = new Label(composite, SWT.NONE);
+		fd_text_username.top = new FormAttachment(label_username, 0, SWT.TOP);
+		FormData fd_label_username = new FormData();
+		fd_label_username.bottom = new FormAttachment(0, 231);
+		fd_label_username.right = new FormAttachment(0, 352);
+		fd_label_username.top = new FormAttachment(0, 199);
+		fd_label_username.left = new FormAttachment(0, 274);
+		label_username.setLayoutData(fd_label_username);
 		label_username.setAlignment(SWT.CENTER);
 		label_username.setFont(SWTResourceManager.getFont("黑体", 12, SWT.NORMAL));
-		label_username.setBounds(274, 199, 78, 32);
 		label_username.setText("用户名");
 		
-		Label label_password = new Label(this, SWT.NONE);
+		Label label_password = new Label(composite, SWT.NONE);
+		fd_text_password.top = new FormAttachment(label_password, 0, SWT.TOP);
+		FormData fd_label_password = new FormData();
+		fd_label_password.bottom = new FormAttachment(0, 290);
+		fd_label_password.right = new FormAttachment(0, 352);
+		fd_label_password.top = new FormAttachment(0, 258);
+		fd_label_password.left = new FormAttachment(0, 274);
+		label_password.setLayoutData(fd_label_password);
 		label_password.setText("密码");
 		label_password.setFont(SWTResourceManager.getFont("黑体", 12, SWT.NORMAL));
 		label_password.setAlignment(SWT.CENTER);
-		label_password.setBounds(274, 258, 78, 32);
 		
-		Label label_tip = new Label(this, SWT.NONE);
-		label_tip.setFont(SWTResourceManager.getFont("黑体", 25, SWT.NORMAL));
-		label_tip.setAlignment(SWT.CENTER);
-		label_tip.setBounds(200, 41, 384, 111);
-		label_tip.setText("用户名为五字符以上的字母数字下划线组合，密码不少于6位");
-		
-		Label warn = new Label(this, SWT.NONE);
+		Label warn = new Label(composite, SWT.NONE);
+		FormData fd_warn = new FormData();
+		fd_warn.bottom = new FormAttachment(0, 252);
+		fd_warn.right = new FormAttachment(0, 658);
+		fd_warn.top = new FormAttachment(0, 229);
+		fd_warn.left = new FormAttachment(0, 119);
+		warn.setLayoutData(fd_warn);
 		warn.setAlignment(SWT.CENTER);
 		warn.setForeground(SWTResourceManager.getColor(SWT.COLOR_RED));
 		warn.setFont(SWTResourceManager.getFont("黑体", 12, SWT.NORMAL));
-		warn.setBounds(295, 229, 217, 23);
-
-		Button button_tosign = new Button(this, SWT.NONE);
-		button_tosign.addSelectionListener(new SelectionAdapter() {
-			@Override
-			public void widgetSelected(SelectionEvent e) {//注册
-				
-				String username=text_username.getText();
-				String password=text_password.getText();
-				
-				
-				if(username.length()<=5||password.length()<6) {//长度条件
-					warn.setForeground(SWTResourceManager.getColor(SWT.COLOR_RED));
-					warn.setText("注册失败");
-					text_username.setFocus();
-					Runnable timer = new Runnable() {
-						@Override
-						public void run() {if(!isDisposed())warn.setText("");}
-					};
-					Display.getDefault().timerExec(3000, timer);
-				}
-				else {
-					 conn=AboutDB.loginDB();
-						Statement Stmt;
-						String sqlQuery;
-						try {//与已有用户名比较，防止重复
-							Stmt = conn.createStatement();
-							sqlQuery="select 用户名 from UserPass";//查询指令
-							Boolean cansign=true;
-							ResultSet rs=Stmt.executeQuery(sqlQuery);
-							while(rs.next()) {
-								if(username.equals(rs.getString(1))){//用户名重复
-									warn.setForeground(SWTResourceManager.getColor(SWT.COLOR_RED));
-									warn.setText("用户名已存在，注册失败");
-									text_username.setFocus();
-									Runnable timer = new Runnable() {
-										@Override
-										public void run() {if(!isDisposed())warn.setText("");}
-									};
-									Display.getDefault().timerExec(3000, timer);
-									cansign=false;
-									break;
-								}
-							}
-							if(cansign.equals(true)) {//满足注册条件，写入数据库
-								sqlQuery="use HMS insert into UserPass values("+username+","+password+")";//写入数据库指令
-								Stmt.executeUpdate(sqlQuery);
-								warn.setForeground(SWTResourceManager.getColor(SWT.COLOR_GREEN));
-								warn.setText("注册成功");
-								Runnable timer = new Runnable() {
-									@Override
-									public void run() {if(!isDisposed())warn.setText("");}
-								};
-								Display.getDefault().timerExec(3000, timer);
-							}
-							
-							
-						} catch (SQLException ee) {warn.setForeground(SWTResourceManager.getColor(SWT.COLOR_RED));warn.setText("无法连接到服务器");}
-						
-				}
-				
-			}
-		});
-		button_tosign.setBounds(422, 319, 80, 27);
-		button_tosign.setText("注册");
-		button_tosign.setFont(SWTResourceManager.getFont("黑体", 11, SWT.NORMAL));
 		
-		Button button_returnenter = new Button(this, SWT.NONE);
-		button_returnenter.addSelectionListener(new SelectionAdapter() {
-			@Override
-			public void widgetSelected(SelectionEvent e) {//返回登陆界面
+		Label label_tip = new Label(composite, SWT.WRAP | SWT.CENTER);
+		FormData fd_label_tip = new FormData();
+		fd_label_tip.top = new FormAttachment(0, 10);
+		fd_label_tip.left = new FormAttachment(0, 58);
+		fd_label_tip.bottom = new FormAttachment(0, 177);
+		fd_label_tip.right = new FormAttachment(0, 719);
+		label_tip.setLayoutData(fd_label_tip);
+		label_tip.setFont(SWTResourceManager.getFont("黑体", 25, SWT.NORMAL));
+		label_tip.setAlignment(SWT.CENTER);
+		label_tip.setText("用户名为五字符以上的字母数字下划线组合，密码不少于6位");
+		
+		Composite composite_Button = new Composite(composite, SWT.NONE);
+		FormData fd_composite_Button = new FormData();
+		fd_composite_Button.right = new FormAttachment(0, 569);
+		fd_composite_Button.top = new FormAttachment(0, 319);
+		fd_composite_Button.left = new FormAttachment(0, 209);
+		composite_Button.setLayoutData(fd_composite_Button);
+				RowLayout rl_composite_Button = new RowLayout(SWT.HORIZONTAL);
+				rl_composite_Button.wrap = false;
+				rl_composite_Button.pack = false;
+				rl_composite_Button.justify = true;
+				composite_Button.setLayout(rl_composite_Button);
+		
+				Button button_tosign = new Button(composite_Button, SWT.NONE);
+				button_tosign.setLayoutData(new RowData(77, SWT.DEFAULT));
+				button_tosign.addSelectionListener(new SelectionAdapter() {
+					@Override
+					public void widgetSelected(SelectionEvent e) {//注册
+						
+						String username=text_username.getText();
+						String password=text_password.getText();
+						
+						
+						if(username.length()<=5||password.length()<6) {//长度条件
+							warn.setForeground(SWTResourceManager.getColor(SWT.COLOR_RED));
+							warn.setText("注册失败");
+							text_username.setFocus();
+							Runnable timer = new Runnable() {
+								@Override
+								public void run() {if(!isDisposed())warn.setText("");}
+							};
+							Display.getDefault().timerExec(3000, timer);
+						}
+						else {
+							 conn=AboutDB.loginDB();
+								Statement Stmt;
+								String sqlQuery;
+								try {//与已有用户名比较，防止重复
+									Stmt = conn.createStatement();
+									sqlQuery="select 用户名 from UserPass where 用户名=" +username+ "";//查询指令
+									Boolean cansign=true;
+									ResultSet rs=Stmt.executeQuery(sqlQuery);
+									while(rs.next()) {
+										if(username.equals(rs.getString(1))){//用户名重复
+											warn.setForeground(SWTResourceManager.getColor(SWT.COLOR_RED));
+											warn.setText("用户名已存在，注册失败");
+											text_username.setFocus();
+											Runnable timer = new Runnable() {
+												@Override
+												public void run() {if(!isDisposed())warn.setText("");}
+											};
+											Display.getDefault().timerExec(3000, timer);
+											cansign=false;
+											break;
+										}
+									}
+									if(cansign.equals(true)) {//满足注册条件，写入数据库
+										sqlQuery="use HMS insert into UserPass values("+username+","+password+")";//写入数据库指令
+										Stmt.executeUpdate(sqlQuery);
+										warn.setForeground(SWTResourceManager.getColor(SWT.COLOR_GREEN));
+										warn.setText("注册成功");
+										Runnable timer = new Runnable() {
+											@Override
+											public void run() {if(!isDisposed())warn.setText("");}
+										};
+										Display.getDefault().timerExec(3000, timer);
+									}
+									
+									
+								} catch (SQLException ee) {warn.setForeground(SWTResourceManager.getColor(SWT.COLOR_RED));warn.setText("无法连接到服务器");}
+								
+						}
+						
+					}
+				});
+				button_tosign.setText("注册");
+				button_tosign.setFont(SWTResourceManager.getFont("黑体", 11, SWT.NORMAL));
 				
-			    close();
-			    try {
-					Enter window = new Enter();
-					window.open();
-				} catch (Exception ee) {
-					ee.printStackTrace();
-				}
-			
-			}
-		});
-		button_returnenter.setText("返回");
-		button_returnenter.setFont(SWTResourceManager.getFont("黑体", 11, SWT.NORMAL));
-		button_returnenter.setBounds(270, 319, 80, 27);
+				Button button_returnenter = new Button(composite_Button, SWT.NONE);
+				button_returnenter.addSelectionListener(new SelectionAdapter() {
+					@Override
+					public void widgetSelected(SelectionEvent e) {//返回登陆界面
+						
+					    close();
+					    try {
+							Enter window = new Enter();
+							window.open();
+						} catch (Exception ee) {
+							ee.printStackTrace();
+						}
+					
+					}
+				});
+				button_returnenter.setText("返回");
+				button_returnenter.setFont(SWTResourceManager.getFont("黑体", 11, SWT.NORMAL));
+		
+
 
 
 		
