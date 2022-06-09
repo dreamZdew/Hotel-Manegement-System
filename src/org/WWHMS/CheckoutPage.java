b7f9a0025cf08740e885ab385a35077e5aae9120
dreamZdew <dreamZdew@outlook.com>
2022-06-09 15:31:47 +0800

    结算界面更新

--- src/org/WWHMS/CheckoutPage.java
+++ src/org/WWHMS/CheckoutPage.java
@@ -16,6 +16,9 @@ import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.custom.StackLayout;
+import org.eclipse.swt.custom.TableCursor;
+import org.eclipse.swt.events.MouseAdapter;
+import org.eclipse.swt.events.MouseEvent;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.layout.RowLayout;
@@ -24,11 +27,16 @@ import org.eclipse.swt.layout.RowData;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.TableColumn;
 import org.eclipse.swt.widgets.TableItem;
+import org.eclipse.swt.widgets.Text;
 
 /*主界面*/
 
 public class CheckoutPage extends Shell {
 	private Table table;
+	private int cursorrow,cursorcol;
+	private Text text;
+	private Text text_1;
+	private Text text_2;
 
 	/**
 	 * Launch the application.
@@ -75,8 +83,8 @@ public class CheckoutPage extends Shell {
 
 		Composite composite_MainofCheck = new Composite(composite, SWT.NONE);
 		composite_MainofCheck.setBackground(SWTResourceManager.getColor(SWT.COLOR_LIST_SELECTION));
-		final StackLayout sl_composite_MainofCheck = new StackLayout();
-		composite_MainofCheck.setLayout(sl_composite_MainofCheck);
+		final StackLayout stackLayoutforCheck = new StackLayout();
+		composite_MainofCheck.setLayout(stackLayoutforCheck);
 
 
 		GridData gd_composite_MainofCheck = new GridData(SWT.CENTER, SWT.CENTER, true, true, 1, 1);
@@ -84,14 +92,14 @@ public class CheckoutPage extends Shell {
 		gd_composite_MainofCheck.heightHint = 401;
 		composite_MainofCheck.setLayoutData(gd_composite_MainofCheck);
 
-		Composite composite_6 = new Composite(composite_MainofCheck, SWT.NONE);
-		composite_6.setBackground(SWTResourceManager.getColor(SWT.COLOR_LIST_SELECTION));
-		RowLayout rl_composite_6 = new RowLayout(SWT.VERTICAL);
-		rl_composite_6.center = true;
-		rl_composite_6.justify = true;
-		composite_6.setLayout(rl_composite_6);
+		Composite composite_1 = new Composite(composite_MainofCheck, SWT.NONE);
+		composite_1.setBackground(SWTResourceManager.getColor(SWT.COLOR_LIST_SELECTION));
+		RowLayout rl_composite_1 = new RowLayout(SWT.VERTICAL);
+		rl_composite_1.center = true;
+		rl_composite_1.justify = true;
+		composite_1.setLayout(rl_composite_1);
 
-		Label lblNewLabel = new Label(composite_6, SWT.NONE);
+		Label lblNewLabel = new Label(composite_1, SWT.NONE);
 		lblNewLabel.setFont(SWTResourceManager.getFont("Microsoft YaHei UI", 12, SWT.NORMAL));
 		lblNewLabel.setAlignment(SWT.CENTER);
 		lblNewLabel.setLayoutData(new RowData(150, 39));
@@ -99,10 +107,20 @@ public class CheckoutPage extends Shell {
 		lblNewLabel.setBackground(SWTResourceManager.getColor(SWT.COLOR_LIST_SELECTION));
 		lblNewLabel.setForeground(SWTResourceManager.getColor(SWT.COLOR_WHITE));
 
-		table = new Table(composite_6, SWT.BORDER | SWT.FULL_SELECTION);
+		table = new Table(composite_1, SWT.BORDER | SWT.FULL_SELECTION);
 		table.setLayoutData(new RowData(672, 237));
 		table.setHeaderVisible(true);
 		table.setLinesVisible(true);
+		
+		final TableCursor cursor = new TableCursor(table, SWT.FULL_SELECTION);//鼠标对表格监听
+		cursor.addMouseListener(new MouseAdapter() {
+			public void mouseDown(MouseEvent e) {
+				cursorrow = table.getSelectionIndex();
+				cursorcol = cursor.getColumn();
+				TableItem t = table.getItem(cursorrow);
+				System.out.println(t.getText(0));
+			}
+		});
 
 		TableColumn tblclmnNewColumn = new TableColumn(table, SWT.NONE);
 		tblclmnNewColumn.setWidth(100);
@@ -132,35 +150,91 @@ public class CheckoutPage extends Shell {
 		tblclmnNewColumn_6.setWidth(100);
 		tblclmnNewColumn_6.setText("订单状态");
 
-		Composite composite_7 = new Composite(composite_MainofCheck, SWT.NONE);
-		composite_7.setBackground(SWTResourceManager.getColor(SWT.COLOR_LIST_SELECTION));
+		Composite composite_2 = new Composite(composite_MainofCheck, SWT.NONE);
+		composite_2.setBackground(SWTResourceManager.getColor(SWT.COLOR_LIST_SELECTION));
+		composite_2.setLayout(new RowLayout(SWT.VERTICAL));
+		
+		Composite composite_21 = new Composite(composite_2, SWT.NONE);
+		composite_21.setLayoutData(new RowData(452, 110));
+		
+		Label lblNewLabel_3 = new Label(composite_21, SWT.NONE);
+		lblNewLabel_3.setBounds(49, 36, 61, 17);
+		lblNewLabel_3.setText("订单号");
+		
+		Label lblNewLabel_4 = new Label(composite_21, SWT.NONE);
+		lblNewLabel_4.setBounds(116, 36, 326, 17);
+		lblNewLabel_4.setText("New Label");
+		
+		Composite composite_22 = new Composite(composite_2, SWT.NONE);
+		composite_22.setLayout(new FillLayout(SWT.HORIZONTAL));
+		composite_22.setLayoutData(new RowData(345, 34));
+		
+		Composite composite_4 = new Composite(composite_22, SWT.NONE);
+		composite_4.setLayout(new RowLayout(SWT.HORIZONTAL));
+		
+		Label lblNewLabel_1 = new Label(composite_4, SWT.NONE);
+		lblNewLabel_1.setText("入住时间");
+		
+		text = new Text(composite_4, SWT.BORDER);
+		
+		Composite composite_4_1 = new Composite(composite_22, SWT.NONE);
+		composite_4_1.setLayout(new RowLayout(SWT.HORIZONTAL));
+		
+		Label lblNewLabel_1_1 = new Label(composite_4_1, SWT.NONE);
+		lblNewLabel_1_1.setText("退房时间");
+		
+		text_1 = new Text(composite_4_1, SWT.BORDER);
+		
+		Composite composite_23 = new Composite(composite_2, SWT.NONE);
+		composite_23.setLayout(new FillLayout(SWT.HORIZONTAL));
+		composite_23.setLayoutData(new RowData(354, SWT.DEFAULT));
+		
+		Label lblNewLabel_2 = new Label(composite_23, SWT.NONE);
+		lblNewLabel_2.setText("New Label");
+		
+		text_2 = new Text(composite_23, SWT.BORDER);
 		composite_MainofCheck.layout();
 
-		Button btnNewButton_6 = new Button(composite_Buttons, SWT.NONE);
-		btnNewButton_6.addSelectionListener(new SelectionAdapter() {
+		Button btnNewButton_1 = new Button(composite_Buttons, SWT.NONE);
+		btnNewButton_1.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent e) {
-				sl_composite_MainofCheck.topControl = composite_6;
-				composite_MainofCheck.layout();
-			}
-		});
-		btnNewButton_6.setForeground(SWTResourceManager.getColor(SWT.COLOR_WHITE));
-		btnNewButton_6.setFont(SWTResourceManager.getFont("黑体", 11, SWT.NORMAL));
-		btnNewButton_6.setBackground(SWTResourceManager.getColor(SWT.COLOR_LINK_FOREGROUND));
-		btnNewButton_6.setText("New Button");
+				table.removeAll();;
+				try {
+					Connection conn = AboutDB.loginDB();
+					PreparedStatement prep = conn.prepareStatement("SELECT 订单编号,下单时间,客人编号,房间编号,入住时间,退房时间,订单状态 FROM OrderList");
+					ResultSet rs = prep.executeQuery();
+					while (rs.next()) {
+						TableItem tableitem = new TableItem(table, SWT.NULL);
+						tableitem.setText(new String[] { rs.getString(1), rs.getString(2), rs.getString(3), rs.getString(4),
+								rs.getString(5), rs.getString(6), rs.getString(7) });
 
-		Button btnNewButton_7 = new Button(composite_Buttons, SWT.NONE);
-		btnNewButton_7.addSelectionListener(new SelectionAdapter() {
-			@Override
-			public void widgetSelected(SelectionEvent e) {
-				sl_composite_MainofCheck.topControl = composite_7;
+					}
+				} catch (SQLException ee) {
+					// TODO 自动生成的 catch 块
+					ee.printStackTrace();
+				}
+				stackLayoutforCheck.topControl = composite_1;
 				composite_MainofCheck.layout();
 			}
 		});
-		btnNewButton_7.setForeground(SWTResourceManager.getColor(SWT.COLOR_WHITE));
-		btnNewButton_7.setFont(SWTResourceManager.getFont("黑体", 11, SWT.NORMAL));
-		btnNewButton_7.setBackground(SWTResourceManager.getColor(SWT.COLOR_LINK_FOREGROUND));
-		btnNewButton_7.setText("New Button");
+		btnNewButton_1.setForeground(SWTResourceManager.getColor(SWT.COLOR_WHITE));
+		btnNewButton_1.setFont(SWTResourceManager.getFont("黑体", 11, SWT.NORMAL));
+		btnNewButton_1.setBackground(SWTResourceManager.getColor(SWT.COLOR_LINK_FOREGROUND));
+		btnNewButton_1.setText("选择订单");
+
+		Button btnNewButton_2 = new Button(composite_Buttons, SWT.NONE);
+		btnNewButton_2.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				stackLayoutforCheck.topControl = composite_2;
+				composite_MainofCheck.layout();
+			}
+		});
+		btnNewButton_2.setForeground(SWTResourceManager.getColor(SWT.COLOR_WHITE));
+		btnNewButton_2.setFont(SWTResourceManager.getFont("黑体", 11, SWT.NORMAL));
+		btnNewButton_2.setBackground(SWTResourceManager.getColor(SWT.COLOR_LINK_FOREGROUND));
+		btnNewButton_2.setText("结算订单");
 
 		try {
 			Connection conn = AboutDB.loginDB();
@@ -178,7 +252,7 @@ public class CheckoutPage extends Shell {
 		}
 		
 		
-		sl_composite_MainofCheck.topControl=composite_6;
+		stackLayoutforCheck.topControl=composite_1;
 		composite_MainofCheck.layout();
 		createContents();
 	}
